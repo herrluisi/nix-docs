b0955a5e0939de0b4921f346b51f96c61fd0538d
Luis Herr <kontakt@luisherr.eu>
2026-04-17 10:30:48 +0200

    init project

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "My NixOS docs with Quartz";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            nodejs_22
+          ];
+          shellHook = ''
+            echo "Quartz dev env ready – node $(node --version)"
+          '';
+        };
+      });
+}
